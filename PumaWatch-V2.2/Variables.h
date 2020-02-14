2b61d09dc665e1935f61394a85372f633cefc856
PumaFPV hp <49276509+PumaFPV@users.noreply.github.com>
2020-02-14 22:44:40 +0100

    added getsketchname

--- PumaWatch-V2.2/Variables.h
+++ PumaWatch-V2.2/Variables.h
@@ -25,3 +25,15 @@ SimpleKalmanFilter Vario(0.1, 0.02, 0.01);
 TinyGPSPlus gps;
 TFT_eSPI tft = TFT_eSPI();   // Invoke library
 TFT_eFEX  fex = TFT_eFEX(&tft);    // Create TFT_eFX object "efx" with pointer to "tft" object
+
+
+void GetSketchName(){
+  
+    String path = __FILE__;
+    int slash = path.lastIndexOf('\x5C');
+    String the_cpp_name = path.substring(slash+1);
+    int dot_loc = the_cpp_name.lastIndexOf('.');
+    Firmware = the_cpp_name.substring(0, dot_loc);
+    Serial.println(Firmware);
+    
+}
